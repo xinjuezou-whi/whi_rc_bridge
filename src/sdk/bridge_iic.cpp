539357111cac4b1b957d7c0dc0a9ab47bfefe5ac
Xinjue <hit_zou@126.com>
2023-11-27 17:52:49 +0800

    minor comment revise

--- src/sdk/bridge_iic.cpp
+++ src/sdk/bridge_iic.cpp
@@ -10,9 +10,6 @@ Written by Xinjue Zou, xinjue.zou@outlook.com
 Apache License Version 2.0, check LICENSE for more information.
 All text above must be included in any redistribution.
 
-Changelog:
-2023-11-22: Initial version
-2022-xx-xx: xxx
 ******************************************************************/
 #include "whi_rc_bridge/bridge_iic.h"
 
