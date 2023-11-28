f58d0a7769545a747b73592f189cfa0ec26b87e3
Xinjue <hit_zou@126.com>
2023-11-28 22:54:29 +0800

    sbus custom baudrate

--- include/whi_rc_bridge/bridge_sbus.h
+++ include/whi_rc_bridge/bridge_sbus.h
@@ -37,12 +37,29 @@ namespace whi_rc_bridge
     protected:
         bool openSerial(const std::string& DeviceAddr, int Baudrate);
         int serialAvailable();
-        int serialRead(char* Buf, unsigned int Len);
+        int serialRead(uint8_t* Buf, unsigned int Len);
         void threadReadSerial();
 
     protected:
         int serial_handle_{ -1 };
         std::thread th_read_;
 	    std::atomic_bool terminated_{ false };
+        uint8_t pre_byte_{ FOOTER };
+        uint8_t msg_buf_[25];
+        int index_{ 0 };
+
+    protected:
+        // SBUS message definitions
+        static constexpr int8_t PAYLOAD_LEN = 23;
+        static constexpr int8_t HEADER_LEN = 1;
+        static constexpr int8_t FOOTER_LEN = 1;
+        static constexpr int8_t NUM_SBUS_CH = 16;
+        static constexpr uint8_t HEADER = 0x0F;
+        static constexpr uint8_t FOOTER = 0x00;
+        static constexpr uint8_t FOOTER2 = 0x04;
+        static constexpr uint8_t CH17_MASK = 0x01;
+        static constexpr uint8_t CH18_MASK = 0x02;
+        static constexpr uint8_t LOST_FRAME_MASK = 0x04;
+        static constexpr uint8_t FAILSAFE_MASK = 0x08;
     };
 } // namespace whi_rc_bridge
--- src/sdk/bridge_sbus.cpp
+++ src/sdk/bridge_sbus.cpp
@@ -13,10 +13,10 @@ All text above must be included in any redistribution.
 ******************************************************************/
 #include "whi_rc_bridge/bridge_sbus.h"
 
+#include <asm/termbits.h>
 #include <fcntl.h>
-#include <stropts.h>
-#include <asm/termios.h>
-#include <asm/ioctl.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 #include <functional>
 #include <iostream>
@@ -52,7 +52,7 @@ namespace whi_rc_bridge
 
     bool SbusBridge::openSerial(const std::string& DeviceAddr, int Baudrate)
     {
-        serial_handle_ = open(DeviceAddr.c_str(), O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK);
+        serial_handle_ = open(DeviceAddr.c_str(), O_RDWR | O_NOCTTY | O_NONBLOCK);
         if (serial_handle_ <= 0)
         {
             return false;
@@ -60,14 +60,33 @@ namespace whi_rc_bridge
         else
         {
             struct termios2 tio;
-            ioctl(serial_handle_, TCGETS2, &tio);
-            tio.c_cflag &= ~CBAUD;
-            tio.c_cflag |= BOTHER;
-            tio.c_ispeed = Baudrate;
-            tio.c_ospeed = Baudrate;
-            ioctl(serial_handle_, TCSETS2, &tio);
+            int rc = ioctl(serial_handle_, TCGETS2, &tio);
+            if (rc)
+            {
+                close(serial_handle_);
+                std::cout << "failed to get termios2" << std::endl;
 
-            return true;
+                return false;
+            }
+            else
+            {
+                // Clear the current output baud rate and fill a new value
+                tio.c_cflag &= ~CBAUD;
+                tio.c_cflag |= BOTHER;
+                tio.c_ospeed = Baudrate;
+                // Clear the current input baud rate and fill a new value
+                tio.c_cflag &= ~(CBAUD << IBSHIFT);
+                tio.c_cflag |= BOTHER << IBSHIFT;
+                tio.c_ispeed = Baudrate;
+                // SBUS
+                tio.c_cflag |= PARENB; // enable parity
+                tio.c_cflag &= ~PARODD; // even parity
+                tio.c_cflag |= CSTOPB; // 2 stop bits
+                // Set new serial port settings via supported ioctl
+                rc = ioctl(serial_handle_, TCSETS2, &tio);
+
+                return true;
+            }
         }
     }
 
@@ -91,7 +110,7 @@ namespace whi_rc_bridge
         }
     }
 
-    int SbusBridge::serialRead(char* Buf, unsigned int Len)
+    int SbusBridge::serialRead(uint8_t* Buf, unsigned int Len)
     {
         return serial_handle_ > 0 ? (int)read(serial_handle_, Buf, Len) : 0;
     }
@@ -103,17 +122,83 @@ namespace whi_rc_bridge
             size_t count = serialAvailable();
             if (count > 0)
             {
-                char rbuff[count];
+                uint8_t rbuff[count];
                 int readNum = serialRead(rbuff, count);
-#ifndef DEBUG
+#ifdef DEBUG
                 std::cout << "read from SBUS: ";
-                for (int i = 0; i < readNum; ++i)
+                for (const auto& it : rbuff)
                 {
-                    std::cout << int(rbuff[i]) << ",";
+                    std::cout << int(it) << ",";
                 }
                 std::cout << std::endl;
 #endif
-                // fetchData(rbuff, readNum);
+                for (const auto& it : rbuff)
+                {
+                    if (index_ == 0)
+                    {
+                        if ((it == HEADER) && ((pre_byte_ == FOOTER) || ((pre_byte_ & 0x0F) == FOOTER2)))
+                        {
+                            msg_buf_[index_++] = it;
+                        }
+                        else
+                        {
+                            index_ = 0;
+                        }
+                    }
+                    else if (index_ < PAYLOAD_LEN + HEADER_LEN)
+                    {
+                        msg_buf_[index_++] = it;
+                    }
+                    else if (index_ < PAYLOAD_LEN + HEADER_LEN + FOOTER_LEN)
+                    {
+                        index_ = 0;
+                        pre_byte_ = it;
+                        if (it == FOOTER || (it & 0x0F) == FOOTER2)
+                        {
+                            // // Grab the channel data
+                            auto ch0 = static_cast<int16_t>(msg_buf_[1] | ((msg_buf_[2] << 8) & 0x07FF));
+                            auto ch1 = static_cast<int16_t>((msg_buf_[2] >> 3) | ((msg_buf_[3] << 5) & 0x07FF));
+                            auto ch2 = static_cast<int16_t>((msg_buf_[3] >> 6) | (msg_buf_[4] << 2) |
+                                ((msg_buf_[5] << 10) & 0x07FF));
+                            auto ch3 = static_cast<int16_t>((msg_buf_[5] >> 1) | ((msg_buf_[6] << 7) & 0x07FF));
+std::cout << "ch0 " << int(ch0) << ", ch1 " << int(ch1) << ", ch2 " << ch2 << ", ch3 " << ch3 << std::endl;
+                            // data_.ch[0] = static_cast<int16_t>(buf_[1] | ((buf_[2] << 8) & 0x07FF));
+                            // data_.ch[1] = static_cast<int16_t>((buf_[2] >> 3) | ((buf_[3] << 5) & 0x07FF));
+                            // data_.ch[2] = static_cast<int16_t>((buf_[3] >> 6) | (buf_[4] << 2) | ((buf_[5] << 10) & 0x07FF));
+                            // data_.ch[3] = static_cast<int16_t>((buf_[5] >> 1) | ((buf_[6] << 7) & 0x07FF));
+                            // data_.ch[4] = static_cast<int16_t>((buf_[6] >> 4) | ((buf_[7] << 4) & 0x07FF));
+                            // data_.ch[5] = static_cast<int16_t>((buf_[7] >> 7) | (buf_[8] << 1) | ((buf_[9] << 9) & 0x07FF));
+                            // data_.ch[6] = static_cast<int16_t>((buf_[9] >> 2) | ((buf_[10] << 6) & 0x07FF));
+                            // data_.ch[7] = static_cast<int16_t>((buf_[10] >> 5) | ((buf_[11] << 3) & 0x07FF));
+                            // data_.ch[8] = static_cast<int16_t>(buf_[12] | ((buf_[13] << 8) & 0x07FF));
+                            // data_.ch[9] = static_cast<int16_t>((buf_[13] >> 3) | ((buf_[14] << 5) & 0x07FF));
+                            // data_.ch[10] = static_cast<int16_t>((buf_[14] >> 6) | (buf_[15] << 2) | ((buf_[16] << 10) & 0x07FF));
+                            // data_.ch[11] = static_cast<int16_t>((buf_[16] >> 1) | ((buf_[17] << 7) & 0x07FF));
+                            // data_.ch[12] = static_cast<int16_t>((buf_[17] >> 4) | ((buf_[18] << 4) & 0x07FF));
+                            // data_.ch[13] = static_cast<int16_t>((buf_[18] >> 7) | (buf_[19] << 1) | ((buf_[20] << 9) & 0x07FF));
+                            // data_.ch[14] = static_cast<int16_t>((buf_[20] >> 2) | ((buf_[21] << 6) & 0x07FF));
+                            // data_.ch[15] = static_cast<int16_t>((buf_[21] >> 5) | ((buf_[22] << 3) & 0x07FF));
+                            // // CH 17
+                            // data_.ch17 = buf_[23] & CH17_MASK;
+                            // // CH 18
+                            // data_.ch18 = buf_[23] & CH18_MASK;
+                            // // Grab the lost frame
+                            // data_.lost_frame = buf_[23] & LOST_FRAME_MASK;
+                            // // Grab the failsafe
+                            // data_.failsafe = buf_[23] & FAILSAFE_MASK;
+                            // //return true;
+                        }
+                        else
+                        {
+                            //return false;
+                        }
+                    }
+                    else
+                    {
+                        index_ = 0;
+                    }
+                    pre_byte_ = it;
+                }
             }
         }
     }
